e2377fd7292dfc65ebc08f7a181535e15b63ba28
Crack-Boyy <mayotyohan@gmail.com>
2025-09-04 15:35:50 +0200

    Last Push

--- Expo-Projet-Exo/app/(tabs)/index.tsx
+++ Expo-Projet-Exo/app/(tabs)/index.tsx
@@ -11,18 +11,7 @@ Alert.alert("Hello " + name);
 }
 return (
   <>
-    <ParallaxScrollView 
-      headerImage={<View style={{ flex: 1, backgroundColor: 'transparent' }} />} 
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}> 
-      <ThemedView style={styles.titleContainer}> 
-        <ThemedText type="title">Détection Platform</ThemedText> 
-      </ThemedView> 
-       
-      <ThemedView style={styles.stepContainer}> 
-        <PlateformDetector /> 
-      </ThemedView> 
-    </ParallaxScrollView> 
-    
+  
     <ParallaxScrollView
       headerImage={<Image source={require('@/assets/images/partial-react-logo.png')} />}
       headerBackgroundColor={{ light: '#aed2f5ff', dark: '#000000ff' }}>
@@ -41,7 +30,22 @@ return (
         <ExperienceCounter/>
       </ThemedView>
     </ParallaxScrollView>
+    
+<ParallaxScrollView 
+      headerImage={<View style={{ flex: 1, backgroundColor: 'transparent' }} />} 
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}> 
+      <ThemedView style={styles.titleContainer}> 
+        <ThemedText type="title">Détection Platform</ThemedText> 
+      </ThemedView> 
+
+      <ThemedView style={styles.stepContainer}> 
+        <PlateformDetector /> 
+      </ThemedView> 
+    </ParallaxScrollView> 
+
   </>
+
+  
 );
 }
 const styles = StyleSheet.create({
